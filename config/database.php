9d2de8aad7692c88d9e141f586f52f4ea07b14a6
SAKA TAKA <aek1214@yahoo.co.jp>
2021-12-22 16:45:26 +0900

    create project!

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+$db = parse_url( env('DATABASE_URL') );
 
 return [
 
@@ -46,13 +47,20 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
+
+            // 'host' => $db['host'],
+            // 'database' => ltrim($db['path'],'/'),
+            // 'username' => $db['user'],
+            // 'password' => $db['pass'],
+
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+
             'charset' => 'utf8mb4',
+            'port' => env('DB_PORT', '3306'),
+            'unix_socket' => env('DB_SOCKET', ''),
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
